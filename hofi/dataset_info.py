bbac28b4710555671d713104c5fccc3b93987319
Arindam Sikdar <as26@iitbbs.ac.in>
2024-11-06 20:15:29 +0000

    Update dataset_info.py

--- hofi/dataset_info.py
+++ hofi/dataset_info.py
@@ -5,18 +5,10 @@ def datasetInfo(dataset_name):
         nb_classes  = 100
     elif dataset_name == "CUB200":
         nb_classes  = 200
-    elif dataset_name == "Dogs":
-        nb_classes  = 120
     elif dataset_name == "Flower102":
         nb_classes  = 102
     elif dataset_name == "NABird":
         nb_classes  = 555
-    elif dataset_name == "PPMI":
-        nb_classes  = 24
-    elif dataset_name == "Stanford40_bb":
-        nb_classes  = 40
-    elif dataset_name == "Food_101":
-        nb_classes = 101
     else:
         raise ValueError(f"Unknown dataset: {dataset_name}. Supported datasets are : Cars, Aircraft")
     
